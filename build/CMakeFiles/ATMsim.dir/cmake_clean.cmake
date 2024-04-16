9afab3eae2c1ceddeee0d831df4e3618a4666372
Roger Garcia <garcciaroger@gmail.com>
2024-04-15 19:59:19 -0500

    Created Main.cpp file and Cmake file to run program

---
+++ build/CMakeFiles/ATMsim.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "ATMsim"
+  "ATMsim.pdb"
+  "CMakeFiles/ATMsim.dir/main.cpp.o"
+  "CMakeFiles/ATMsim.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ATMsim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
