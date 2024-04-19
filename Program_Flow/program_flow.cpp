8943e35a33112b66c8de8f92ae054027dbc05e11
Roger Garcia <garcciaroger@gmail.com>
2024-04-19 16:22:21 -0500

    Worked on program flow

--- Program_Flow/program_flow.cpp
+++ Program_Flow/program_flow.cpp
@@ -15,6 +15,15 @@ void Flow::login() {
 
 void Flow::main_menu() {
     // Implementation of main menu
+    int option;
+    std::cout << "     Main Menu     " << std::endl;
+    std::cout << "===================" << std::endl;
+    std::cout << "1. Deposit " << std::endl;
+    std::cout << "2. Withdraw " << std::endl;
+    std::cout << "3. Check Balance" << std::endl;
+    std::cout << "4. Exit " << std::endl;
+    std::cout << "===================" << std::endl;
+    std::cout << "Enter Option"; std::cin >> option;
 }
 
 void Flow::start_program(){
